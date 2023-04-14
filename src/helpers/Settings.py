449f2456fe57409e80ea6166f78ba917d9bb655f
Steven Laczko <stevendlaczko@gmail.com>
2023-04-13 20:30:22 -0400

    Made new settings class with the ability to load settings lazily

---
+++ src/helpers/Settings.py
@@ -0,0 +1,72 @@
+import json
+from configparser import ConfigParser
+
+
+def lazy_property(func):
+    attr_name = func.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, func(self))
+        return getattr(self, attr_name)
+
+    return _lazy_property
+
+
+class Settings:
+    _instance = None
+    
+    def __new__(cls, config_file='settings.json'):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._config_file = config_file
+            cls._instance._cache_settings()
+        return cls._instance
+
+    def _cache_settings(self):
+        with open(self._config_file, 'r') as f:
+            self._config = json.load(f)
+
+        self._id_name_dict = {int(k): v for k, v in self._config['id_name_dict']}
+        self._ignore_list = {int(k): v for k, v in self._config['ignore_list']}
+        self._log_level = self._config['log_level']
+        self._max_context_words = self._config['conversation']['max_context_words']
+        self._max_convo_words = self._config['conversation']['max_convo_words']
+        self._max_memory_words = self._config['conversation']['max_memory_words']
+        self._num_messages_per_request = self._config['conversation']['num_messages_per_request']
+        self._context_path = self._config['conversation']['num_messages_per_request']
+
+    @property
+    def id_name_dict(self):
+        return self._id_name_dict
+
+    @property
+    def ignore_list(self):
+        return self._ignore_list
+
+    @property
+    def log_level(self):
+        return self._log_level
+
+    @property
+    def max_context_words(self):
+        return self._max_context_words
+
+    @property
+    def max_convo_words(self):
+        return self._max_convo_words
+
+    @property
+    def max_memory_words(self):
+        return self._max_memory_words
+
+    @property
+    def num_messages_per_request(self):
+        return self._num_messages_per_request
+
+    @property
+    def context_path(self):
+        return self._context_path
+
+settings = Settings()
