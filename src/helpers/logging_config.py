72ee8930b5b44cbb1984c73d1bf13c9c548ca3a8
Steven Laczko <stevendlaczko@gmail.com>
2024-07-02 12:04:20 -0400

    Added stdout handler to logger

--- src/helpers/logging_config.py
+++ src/helpers/logging_config.py
@@ -1,27 +1,37 @@
 import logging
 import json
+import sys
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
 with open("data/settings.json", 'r') as f:
     settings = json.load(f)
 
-if settings["log_level"] == "info":
-    log_level = logging.INFO
-elif settings["log_level"] == "error":
-    log_level = logging.ERROR
-elif settings["log_level"] == "debug":
-    log_level = logging.DEBUG
-else:
-    log_level = logging.WARNING
+log_dict = {
+    "info": logging.INFO,
+    "error": logging.ERROR,
+    "debug": logging.DEBUG,
+    "warning": logging.WARNING
+}
+log_level = log_dict[settings["log_level"]]
 logger = logging.getLogger("main")
 logger.setLevel(log_level)
+
+# format for log messages
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
+
+# print to stdout
+stdHandler = logging.StreamHandler(sys.stdout)
+stdHandler.setLevel(logging.WARNING)
+stdHandler.setFormatter(formatter)
+logger.addHandler(stdHandler)
+
 # Create a rotating file handler
 handler = RotatingFileHandler(filename=f'logs/boris.log', maxBytes=20000, backupCount=5)
 # Set the log level
 handler.setLevel(log_level)
 # Create a formatter and add it to the handler
-formatter = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
 logger.info(f"Log level is {log_level}")
